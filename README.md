1dd227de42e051f71599d49c8efd80278e9b728e
Vivian <eeywy2@nottingham.ac.uk>
2026-02-11 23:15:24 +0000

    clean gh-pages branch

---
+++ README.md
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+Priority: Master 
+=======
+Priority: Devel 
+>>>>>>> devel
+User 2 was here 
+<<<<<<< HEAD
+I want it that way 
+
---
+++ worksheet4/Calc/adder.h
@@ -0,0 +1,11 @@
+﻿#ifdef maths_STATIC
+#define MATHSLIB_API // 静态链接时，宏为空
+#else
+#ifdef MATHS_EXPORTS
+#define MATHSLIB_API __declspec(dllexport)
+#else
+#define MATHSLIB_API __declspec(dllimport)
+#endif
+#endif
+
+MATHSLIB_API int add(int a, int b);
