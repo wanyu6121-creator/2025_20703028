5e380ee38753442ad4c5cd5c2d9446f287a70d90
Vivian <eeywy2@nottingham.ac.uk>
2026-02-11 22:41:01 +0000

    doxyfile upload

---
+++ worksheet5/adder.h
@@ -0,0 +1,18 @@
+﻿/**
+ * @file
+ * This file contains the declarations of all exported functions in the maths library.
+ */
+
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/**
+ * Adding function
+ * * This function adds two numbers
+ * @param a is the first number
+ * @param b is the second number
+ * @return sum of a and b
+ */
+int add(int a, int b);
+
+#endif
